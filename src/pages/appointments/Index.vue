f2564b79d71bb245b8de8b1037f8ec8305f03f14
Cristian Neagu <cristian.ion.neagu@gmail.com>
2019-06-09 18:10:14 +0300

    Kalendar Integration

--- src/pages/appointments/Index.vue
+++ src/pages/appointments/Index.vue
@@ -1,6 +1,6 @@
 <template>
   <q-page class="flex flex-center">
-    Appointments
+    <kalendar class="full-width" :configuration="calendar_settings" :appointments="appointments"/>
   </q-page>
 </template>
 
@@ -8,7 +8,27 @@
 </style>
 
 <script>
+import { Kalendar } from 'kalendar-vue'
+import 'kalendar-vue/dist/KalendarVue.css'
+
 export default {
-  name: 'PageIndex'
+  name: 'PageIndex',
+  components: {
+    Kalendar
+  },
+  data () {
+    return {
+      appointments: [],
+      calendar_settings: {
+        style: 'material_design', // ['flat_design', 'material_design']
+        view_type: 'Month', // ['Month', 'Day']
+        split_value: 20, // Value % 60 === 0
+        cell_height: 20, // !isNaN(Value)
+        scrollToNow: true, // Boolean
+        current_day: new Date(), // Valid date
+        military_time: true // Boolean
+      }
+    }
+  }
 }
 </script>
